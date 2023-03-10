ff959cf5609fc79433875dc45d2a7d0ab254e55a
Marc <marc@mustbe.nl>
2023-03-10 14:23:24 +0100

    Initial commit

---
+++ Casks/freefilesync.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+cask 'freefilesync' do
+  version '12.0'
+  sha256 '777472f81f992c3f2e1abf4ac9c8ada04221cf55167d927a4e4595fe1f9e9042'
+
+  url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
+  name 'FreeFileSync'
+  desc 'Folder comparison and synchronization software'
+  homepage 'https://www.freefilesync.org/'
+
+  auto_updates true
+
+  pkg "FreeFileSync_#{version}.pkg"
+end
