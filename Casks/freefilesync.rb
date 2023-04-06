c06ee99075fb974ffc30a0fda2b1a2f438706444
Joseph Mearman <joseph@mearman.co.uk>
2023-04-06 07:39:44 +0100

    correct version number for hash

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 cask 'freefilesync' do
-  version '12.0'
+  version '12.2'
   sha256 '777472f81f992c3f2e1abf4ac9c8ada04221cf55167d927a4e4595fe1f9e9042'
 
   url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
